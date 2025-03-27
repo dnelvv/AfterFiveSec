9a2961386864462429100e3670514fc4a3035fae
dnelvv <91218302+dnelvv@users.noreply.github.com>
2025-03-27 19:46:44 +0300

    Правки

--- src/test/java/org/example/afterfivesec/MainPageTest.java
+++ src/test/java/org/example/afterfivesec/MainPageTest.java
@@ -5,9 +5,12 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -15,12 +18,13 @@ import java.time.Duration;
 public class MainPageTest {
     private WebDriver driver;
 
-
     @BeforeEach
     public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.setPageLoadStrategy(PageLoadStrategy.EAGER);
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
 
     @AfterEach
@@ -32,6 +36,7 @@ public class MainPageTest {
     public void visableAfterFiveSec() {
         driver.get("https://demoqa.com/dynamic-properties");
         WebDriverWait waitingForVisable = new WebDriverWait(driver, Duration.ofSeconds(5));
-        driver.findElement(By.cssSelector("#visibleAfter"));
+        waitingForVisable.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#visibleAfter")));
+        driver.findElement(By.cssSelector("#visibleAfter")).click();
     }
 }
